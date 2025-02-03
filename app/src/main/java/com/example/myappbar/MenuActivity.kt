b27b706ec9f67447296d0ec6bae96fadffb0e3ca
pateeh <fatihazzam33@gmail.com>
2025-02-03 13:12:45 +0700

    menambahkan aksi untuk menangani searchView tidak terjadi apapun ketika searchView diisi dan ditekan enter

--- app/src/main/java/com/example/myappbar/MenuActivity.kt
+++ app/src/main/java/com/example/myappbar/MenuActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myappbar
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,6 +19,14 @@ class MenuActivity : AppCompatActivity() {
 
         with(binding) {
             searchView.setupWithSearchBar(searchBar)
+            searchView
+                .editText
+                .setOnEditorActionListener{textView, actionId, event ->
+                    searchBar.setText(searchView.text)
+                    searchView.hide()
+                    Toast.makeText(this@MenuActivity, searchView.text, Toast.LENGTH_SHORT).show()
+                    false
+                }
         }
     }
  }
